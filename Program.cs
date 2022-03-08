0bccde939ef944ba6ed50101631f58467b87de46
STUDENT <STUDENT@DESKTOP-1IC7LVA>
2022-03-08 14:28:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FisierConfigurari
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Utilizare valori din fisierul de configurari
+            string titlu = ConfigurationManager.AppSettings.Get("TitluAplicatie");
+            Console.WriteLine(titlu);
+            Console.ReadKey();
+        }
+    }
+}
